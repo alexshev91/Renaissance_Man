3f250d3a0561607235b57fe58c7b718e235d4c08
alexshev91 <alexshev91@yahoo.com>
2014-09-24 20:59:51 -0700

    added ajax for new comments created

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
 
 
-$('.commentbutton1').on('click', function(event){
-  event.preventDefault();
-  $(this).nextAll(".commentdiv1").first().toggle();
-});
+  $('.commentbutton1').on('click', function(event){
+    event.preventDefault();
+    $(this).nextAll(".commentdiv1").first().toggle();
+  });
+
+  $(".new_comment").on("submit", function(event) {
+    event.preventDefault();
+      // debugger;
+      var action = this.attributes.action.value;
+      var post = $(this);
+      var text = post.find("textarea").val();
+      // var params = {content: text};
+      console.log(action);
+      $.ajax({
+        type: 'post',
+        url: action,
+        data: {comment: {content: text}}
+        }
+      ).success(function(data) {
+        // alert(data.content);
+        // debugger;
+        post.prepend("<div class=\"well well-sm\">" + data.name + data.content + "</div >");
+      });
+  });
+
+
+
 });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,25 +7,17 @@ class CommentsController < ApplicationController
     puts x
     @user = @current_user
 
-    # @y = User.find(@user.id)
-    # def increaseScore(user)
+
       old_score = @user[x]
       @user[x] = old_score + 5
       @user.save
-    # end
 
-    # increaseScore(@y)
-
- # puts "thiasdfasdlfasdl;fkjal;sdfkjals;d ;laskdfl;asjkdf;lasdkjf ;l;asdl;kfjasd;lfkj a;sdkfjlasdlk jals l;aksdfj;l aksdjfl; aksdjf ;laskdfj;laskdfj; lkja ;alsdkfjl;asdkfj ;alsdkjf ;alsdk fj;asldkjf ;alsdkfj "
- #    puts @user[x]
- #    puts @user.inspect
-    # @user = @current_user
-    # old_cat_score = @user.cat
-    # @user.cat = old_cat_score + 5
     new_params = comment_params
     new_params[:user_id] = @user.id
-    @parent.comments.create(new_params)
-    redirect_to renaissance_path
+
+   render json:  @parent.comments.create(new_params).as_json.merge(name: @user.name)
+
+
   end
 
   def find_parent
